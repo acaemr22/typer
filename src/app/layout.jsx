822d83f25ee46b72ad0ac27434f6e8409ba0f6f2
Emre Açar <ea517645@gmail.com>
2023-08-05 10:23:03 +0300

    refactor: integrate redux store with the application

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,13 @@
 import "./globals.css";
 import * as React from "react";
+import Providers from "@/components/Providers";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="pt-8">{children}</body>
+      <body className="pt-8">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
