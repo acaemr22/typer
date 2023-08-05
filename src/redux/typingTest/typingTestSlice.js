5be40120d1051e39f95f802ed55f60275e5dfc9c
Emre Açar <ea517645@gmail.com>
2023-08-05 12:33:02 +0300

    feat: use redux thunk middleware to create a wordList fetching function from the api

--- src/redux/typingTest/typingTestSlice.js
+++ src/redux/typingTest/typingTestSlice.js
@@ -1,4 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchWordList = createAsyncThunk(
+  "typingTest/fetchWordList",
+  async ({ wordListType }) => {
+    const res = await fetch(`http://localhost:3000/api/${wordListType}`);
+    const data = await res.json();
+    return data;
+  }
+);
 
 export const typingTestSlice = createSlice({
   name: "typingTest",
@@ -7,21 +17,34 @@ export const typingTestSlice = createSlice({
     WPM: 0,
     completedCount: 0,
     input: "",
+    wordList: [],
+    fetchWordList: {
+      status: "idle",
+    },
   },
   reducers: {
     handleInputChange: (state, action) => {
-      const input = action.payload
+      const input = action.payload;
 
-      if(input !== " " && input.slice(-1) !== " ") {
-        state.input = input
+      if (input !== " " && input.slice(-1) !== " ") {
+        state.input = input;
+      } else {
+        state.input = "";
       }
+    },
+  },
 
-      else {
-        state.input = ""
-      }
-      
-    }
+  extraReducers: (builder) => {
+    builder.addCase(fetchWordList.fulfilled, (state, action) => {
+      state.status = "succeeded";
+      state.wordList = action.payload;
+    });
+    builder.addCase(fetchWordList.rejected, (state, action) => {
+      state.status = "failed";
+      state.wordList = action.error.message;
+    });
   },
 });
 
 export default typingTestSlice.reducer;
+export const { handleInputChange } = typingTestSlice.actions;
