d7db115745951b9a6dd14d0e10849a6c68cb8fd5
Emre Açar <ea517645@gmail.com>
2023-08-13 10:23:14 +0300

    feat: add footer to the site

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,14 @@
 import "./globals.css";
 import * as React from "react";
 import Providers from "@/components/Providers";
+import Footer from "@/components/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="pt-8">
         <Providers>{children}</Providers>
+        <Footer />
       </body>
     </html>
   );
